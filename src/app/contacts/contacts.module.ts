190948c817675c4479c64986b3f621e83ed7f8e4
Karol Ciak <ciakkarol@hotmail.com>
2023-01-21 13:53:32 +0100

    add header component

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ContactsListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [ContactsListComponent],
 })
 export class ContactsModule {}
