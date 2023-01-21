9e378ebd5c46cea727de0bee54d9ffabccc1a4a0
Karol Ciak <ciakkarol@hotmail.com>
2023-01-21 12:18:46 +0100

    add data binding

---
+++ src/app/data/contacts-data.ts
@@ -0,0 +1,11 @@
+export const CONTACTS = [
+  { id: 1, surname: 'Kowalski', firstName: 'Jan', phoneNumber: '489958462' },
+  { id: 2, surname: 'Kowalska', firstName: 'Anna', phoneNumber: '489326462' },
+  { id: 3, surname: 'Nowak', firstName: 'Maria', phoneNumber: '489326462' },
+  {
+    id: 4,
+    surname: 'Nowakowski',
+    firstName: 'Marek',
+    phoneNumber: '489326462',
+  },
+];
