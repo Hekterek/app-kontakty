bb6d3d7a627348cc9dcab9ef58fcd226e8ea3145
Karol Ciak <ciakkarol@hotmail.com>
2023-01-21 15:40:57 +0100

    add routing

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ContactsListComponent],
-  imports: [CommonModule, SharedModule],
-  exports: [ContactsListComponent],
+  declarations: [ContactsListComponent, ContactDetailsComponent],
+  imports: [CommonModule, SharedModule, RouterModule],
+  exports: [ContactsListComponent, ContactDetailsComponent],
 })
 export class ContactsModule {}
