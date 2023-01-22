a895af7f1089e052baa1cb909ea2c9944dd499fd
Karol Ciak <ciakkarol@hotmail.com>
2023-01-22 11:40:24 +0100

    add material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactsModule } from './contacts/contacts.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ContactsModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, ContactsModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -4,10 +4,18 @@ import { ContactsListComponent } from './contacts-list/contacts-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { ContactDetailsComponent } from './contact-details/contact-details.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ContactsListComponent, ContactDetailsComponent],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   exports: [ContactsListComponent, ContactDetailsComponent],
 })
 export class ContactsModule {}
